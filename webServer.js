921743655c2757c493f6833f30afdda642dafa06
Maude <whippou@gmail.com>
2018-08-29 21:06:47 -0400

    Port problem?

--- webServer.js
+++ webServer.js
@@ -35,4 +35,4 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(3000, () => console.log('Web server listening; Head over to localhost:3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Web server listening; Head over to localhost:3000'))
